e53f6cd58853c6f0aec0ce881ef30a525f6d462b
callum thomson <callumthom11@gmail.com>
2015-08-21 23:56:31 +0100

    Added settings for creating release builds and updated the packageing script to copy both the debug and release librarys

--- Package.bat
+++ Package.bat
@@ -17,4 +17,5 @@ xcopy "Unknown 1.0++\*.h" "Unknown++\include" /Y
 
 xcopy "Unknown 1.0++\*.cpp" "Unknown++\src" /Y
 
-copy "Debug\Unknown 1.0++.lib" "Unknown++\lib\x86\Unknown++.lib"
+copy "Debug\Unknown 1.0++.lib" "Unknown++\lib\x86\Unknown++ - Debug.lib"
+copy "release\Unknown 1.0++.lib" "Unknown++\lib\x86\Unknown++.lib"
