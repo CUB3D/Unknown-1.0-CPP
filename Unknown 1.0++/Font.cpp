ef4d0d5c5ae17b86ffda438c77451d713762abf0
CUB3D <callumthom11@gmail.com>
2015-11-28 19:39:18 +0000

    Added support for fonts based on single spritesheets
    Fonts should consist of a single image devided up into squares of equal
    size each placed in one horizontal line. This can be loaded as an image
    and passed to a font constructor as the first argument. The next argument
    should be a string containg the letter that each image represents in the
    same order as the image. The final argument is the size of each image.

---
+++ Unknown 1.0++/Font.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h"
+#include "Font.h"
+
+#include <iostream>
+
+Unknown::Graphics::Font::Font(Image* fontSheet, const std::string layout, const int charSize) : fontSheet(fontSheet), layout(layout), charSize(charSize)
+{
+}
+
+void Unknown::Graphics::Font::drawChar(const char character, const int x, const int y)
+{
+	fontSheet->textureRect.w = this->charSize;
+	fontSheet->textureRect.h = this->charSize;
+
+	int position = layout.find(character);
+
+	SDL_Rect clip;
+	clip.x = position * this->charSize;
+	clip.y = 0;
+	clip.w = this->charSize;
+	clip.h = this->charSize;
+
+	this->fontSheet->render(x, y, 0, &clip);
+}
+
+void Unknown::Graphics::Font::drawString(const std::string string, const int x, const int y)
+{
+	for (int i = 0; i < string.size(); i++)
+	{
+		this->drawChar(string.c_str()[i], x + (i * this->charSize), y);
+	}
+}
+
--- Unknown Test/Unknown Test.cpp
+++ Unknown Test/Unknown Test.cpp
@@ -16,68 +16,22 @@
 #include "Physics.h"
 #include "Loader.h"
 #include "Animation.h"
+#include "Font.h"
 
-Unknown::Graphics::Image player("Player.png");
-Unknown::Graphics::ImageSprite playerSprite(10, 10, &player);
-
-int i = 0;
+Unknown::Graphics::Image font_img("Font.png");
+Unknown::Graphics::Font font(&font_img, "AB", 16);
 
 void render()
 {
+	font.drawString("AAABBABB", 10, 10);
 }
 
 void update()
 {
-	i++;
-
-	if (i == 200)
-	{
-		std::cout << "Removing listener" << std::endl;
-
-		UK_REMOVE_MOUSE_LISTENER("Test_listener");
-		UK_REMOVE_MOUSE_LISTENER("Test_listener_exter");
-
-		UK_REMOVE_KEY_LISTENER("Test_listener");
-		UK_REMOVE_KEY_LISTENER("Test_listener_exter");
-	}
-}
-
-class Test
-{
-public:
-	Test()
-	{
-		UK_ADD_MOUSE_LISTENER_INTERNAL(onMouseClick, "Test_listener");
-		UK_ADD_KEY_LISTENER_INTERNAL(onKey, "Test_listener");
-	}
-
-	void onMouseClick(Unknown::MouseEvent evnt)
-	{
-		std::cout << "Click" << std::endl;
-	}
-
-	void onKey(Unknown::KeyEvent evnt)
-	{
-		std::cout << "Key" << std::endl;
-	}
-};
-
-void onMouseClick(Unknown::MouseEvent evnt)
-{
-	std::cout << "Click_internal" << std::endl;
-}
-
-void onKey(Unknown::KeyEvent evnt)
-{
-	std::cout << "Key_internal" << std::endl;
 }
 
 void init()
 {
-	Test t;
-
-	UK_ADD_KEY_LISTENER_EXTERNAL(onKey, "Test_listener_exter");
-	UK_ADD_MOUSE_LISTENER_EXTERNAL(onMouseClick, "Test_listener_exter");
 }
 
 int _tmain(int argc, _TCHAR* argv[])
