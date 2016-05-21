7ac016248182a417020cb3ac292d5704ef9b1094
CUB3D <callumthom11@gmail.com>
2016-05-21 15:39:55 +0100

    Updated env.sh to move to corrent directory before building

--- tools/env.sh
+++ tools/env.sh
@@ -1,7 +1,7 @@
 export UNKNOWN_PROJECT_ROOT=$(pwd)
 
 alias proot="cd  $UNKNOWN_PROJECT_ROOT"
-alias test=./tools/test/test.sh
-alias build=./tools/build/cbuild.py
-alias build_engine="build ECORE-LINUX"
-alias build_test="build ETEST-LINUX"
+alias test="$(echo $UNKNOWN_PROJECT_ROOT)/tools/test/test.sh"
+alias build="$(echo $UNKNOWN_PROJECT_ROOT)/tools/build/cbuild.py"
+alias build_engine="proot && build ECORE-LINUX"
+alias build_test="proot && build ETEST-LINUX"
