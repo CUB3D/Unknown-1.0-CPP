38797ad5db267388e1f618fb9dc838816aa70c69
cub3d <callumthom11@gmail.com>
2016-05-28 20:19:35 +0100

    Updated the setup to install all of the required packages (on debian at least) and fixed issue where ETEST-LINUX would fail on first build

---
+++ tools/setup/runAgainstFile.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3.4
+
+import sys
+import os
+import locale
+from dialog import Dialog
+
+locale.setlocale(locale.LC_ALL, '')
+
+d = Dialog(dialog="dialog")
+
+d.gauge_start("Installing stuff", percent=0)
+
+file = open(sys.argv[1], "r")
+command = sys.argv[2]
+mode = sys.argv[3]
+
+percent = 0.0
+
+lines = []
+
+for line in file:
+    lines.append(line.rstrip("\n"))
+
+
+if mode == "compound" or mode == "c":
+    fullCommand = command + " " + (" ".join(lines))
+    os.system(fullCommand + " > /dev/null 2>&1")
+    d.gauge_update(100)
+elif mode == "individual" or mode == "i":
+    done = 0
+    for line in lines:
+        os.system(command + " " + line + " > /dev/null 2>&1")
+        done += 1
+        percent = done / len(lines) * 100
+        d.gauge_update(int(percent))
--- tools/setup/setup.sh
+++ tools/setup/setup.sh
@@ -3,3 +3,5 @@
 mkdir "$(echo $UNKNOWN_PROJECT_ROOT)/Libs/RapidJSON"
 
 ln -s "$(echo $UNKNOWN_PROJECT_ROOT)/tools/libs/third_party/rapidJSON/include" "$(echo $UNKNOWN_PROJECT_ROOT)/Libs/RapidJSON/include"
+
+sudo "$(echo $UNKNOWN_PROJECT_ROOT)/tools/setup/runAgainstFile.py" "$(echo $UNKNOWN_PROJECT_ROOT)/tools/setup/requiredPackages.list" "sudo apt -y install" i
