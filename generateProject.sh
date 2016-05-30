ee38fd74cf30b253e4a20c624a0dfd13797cd89b
cub3d <callumthom11@gmail.com>
2016-05-30 21:51:46 +0100

    Testing script now added to generated projects

--- generateProject.sh
+++ generateProject.sh
@@ -18,3 +18,6 @@ cp UK_SRC/headers/* "$1/Libs/Unknown/headers"
 printf '{ \n    "Width":512,\n    "Height":512,\n    "Title":"Test"\n}\n' > "$1"/Config.json
 
 printf '#include "Unknown.h"\n\nint main(int argc, char** argv)\n{\n    UK_CREATE_WINDOW();\n    UK_INIT_GAME();\n    return 0;\n}\n' > "$1"/src/Game.cpp
+
+printf '#!/bin/bash\nexport LD_LIBRARY_PATH="."\n./a.out\n' > "$1/test.sh"
+chmod +x "$1/test.sh"
