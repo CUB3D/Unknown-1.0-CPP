600395ea18a995987c3d0b4035b11f64d8903820
CUB3D <callumthom11@gmail.com>
2017-05-27 18:16:16 +0100

    Project now uses GNU make as a build system

---
+++ Makefile
@@ -0,0 +1,11 @@
+make=make
+
+ecore:
+	$(make) -C Unknown\ 1.0++/
+
+test:
+	$(make) -C Unknown\ Test/
+
+all:
+	$(make) -C Unknown\ 1.0++/
+	$(make) -C Unknown\ Test/
--- tools/test/test.sh
+++ tools/test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH="."
+export LD_LIBRARY_PATH="$(echo $UNKNOWN_PROJECT_ROOT)/Output/lib/linux/."
 cd "Unknown Test"
 ./a.out
 cd ..
