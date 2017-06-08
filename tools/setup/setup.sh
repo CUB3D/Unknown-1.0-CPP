e9951aa4c99fdbcc2fce44bd37b86f4effdf9cce
CUB3D <callumthom11@gmail.com>
2017-06-08 19:07:35 +0100

    Fixed issues with first time builds

--- tools/setup/setup.sh
+++ tools/setup/setup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-mkdir "$(echo $UNKNOWN_PROJECT_ROOT)/Libs/RapidJSON"
-
-ln -s "$(echo $UNKNOWN_PROJECT_ROOT)/tools/libs/third_party/rapidJSON/include" "$(echo $UNKNOWN_PROJECT_ROOT)/Libs/RapidJSON/include"
-
-sudo "$(echo $UNKNOWN_PROJECT_ROOT)/tools/setup/runAgainstFile.py" "$(echo $UNKNOWN_PROJECT_ROOT)/tools/setup/requiredPackages.list" "sudo apt -y install" i
+ln -s "$(echo $UNKNOWN_PROJECT_ROOT)/tools/libs/third_party/rapidJSON" "$(echo $UNKNOWN_PROJECT_ROOT)/Libs/RapidJSON"
