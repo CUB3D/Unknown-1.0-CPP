b7dccac059418d47d01051872600371dd30940c7
CUB3D <callumthom11@gmail.com>
2018-07-28 11:42:15 +0100

    Fixed cmake scripts to find libs on windows

--- tools/build/cmake-scripts/FindSDL2_image.cmake
+++ tools/build/cmake-scripts/FindSDL2_image.cmake
@@ -105,6 +105,7 @@ FIND_PATH(SDL2_IMAGE_INCLUDE_DIR SDL_image.h
 	/opt/local # DarwinPorts
 	/opt/csw # Blastwave
 	/opt
+	${PROJECT_SOURCE_DIR}/Libs/SDL2_image
 )
 
 # Lookup the 64 bit libs on x64
@@ -123,6 +124,7 @@ IF(CMAKE_SIZEOF_VOID_P EQUAL 8)
 		/opt/local
 		/opt/csw
 		/opt
+		${PROJECT_SOURCE_DIR}/Libs/SDL2_image
 	)
 # On 32bit build find the 32bit libs
 ELSE(CMAKE_SIZEOF_VOID_P EQUAL 8)
@@ -140,6 +142,7 @@ ELSE(CMAKE_SIZEOF_VOID_P EQUAL 8)
 		/opt/local
 		/opt/csw
 		/opt
+		${PROJECT_SOURCE_DIR}/Libs/SDL2_image
 	)
 ENDIF(CMAKE_SIZEOF_VOID_P EQUAL 8)
 
