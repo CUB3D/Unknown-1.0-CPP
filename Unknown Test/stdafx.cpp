4cca79ffe55d0939aef3e3b456497ee2fce17161
CUB3D <callumthom11@gmail.com>
2018-07-28 01:03:04 +0100

    Fixed issue with windows specific fix, now only applied on windows

--- Unknown Test/stdafx.cpp
+++ Unknown Test/stdafx.cpp
@@ -7,9 +7,11 @@
 // TODO: reference any additional headers you need in STDAFX.H
 // and not in this file
 
+#ifdef WIN32
 FILE _iob[] = { *stdin, *stdout, *stderr };
 
 extern "C" FILE * __cdecl __iob_func(void)
 {
 	return _iob;
 }
+#endif
