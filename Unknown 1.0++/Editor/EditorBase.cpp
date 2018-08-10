63fe703518200ce423f9cc734baf3a4e106e5bde
CUB3D <callumthom11@gmail.com>
2018-08-10 15:58:29 +0100

    Switched to FFP ogl, moving to PFP next

---
+++ Unknown 1.0++/Editor/EditorBase.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cub3d on 08/08/18.
+//
+
+#include "EditorBase.h"
---
+++ Unknown 1.0++/Editor/EditorBase.h
@@ -0,0 +1,16 @@
+//
+// Created by cub3d on 08/08/18.
+//
+
+#ifndef UNKNOWN_DEVELOPMENT_TOOL_EDITORBASE_H
+#define UNKNOWN_DEVELOPMENT_TOOL_EDITORBASE_H
+
+
+
+class EditorBase {
+
+};
+
+
+
+#endif //UNKNOWN_DEVELOPMENT_TOOL_EDITORBASE_H
