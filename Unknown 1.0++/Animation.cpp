f245e3b59318434e2d961951b62ffabec81fd0f2
CUB3D <callumthom11@gmail.com>
2018-08-12 01:35:54 +0100

    Animated entity support

--- Unknown 1.0++/Animation.cpp
+++ Unknown 1.0++/Animation.cpp
@@ -5,7 +5,7 @@
 
 #include "Image.h"
 
-void Unknown::Graphics::Animation::addFrame(Unknown::Graphics::Image* frame, int delayms)
+void Unknown::Graphics::Animation::addFrame(std::shared_ptr<Image> frame, int delayms)
 {
 	AnimationFrame animationFrame;
 
@@ -36,16 +36,56 @@ void Unknown::Graphics::Animation::draw(const int x, const int y, const double a
 		hasInit = true;
 	}
 
-	currentFrame.frameImage->render(x, y, angle);
+	if(currentFrame.frameImage)
+		currentFrame.frameImage->render(x, y, angle);
 
 	if (frameTimer.isTickComplete())
 	{
 		currentFrameIndex++;
 
-		if ((unsigned int) currentFrameIndex == frames.size())
-		{
+		if ((unsigned int) currentFrameIndex == frames.size() && loop)
 			currentFrameIndex = 0;
-		}
+
+		frameTimer.timerSpeed = frames[currentFrameIndex].delayms;
+	}
+}
+
+Unknown::Graphics::Animation::Animation() : currentFrameIndex(0), loop(false), done(false) {
+
+}
+
+void Unknown::Graphics::Animation::drawNoAdvance(const int x, const int y, const double angle) const {
+	AnimationFrame currentFrame = frames[currentFrameIndex];
+
+	if(currentFrame.frameImage)
+		currentFrame.frameImage->render(x, y, angle);
+}
+
+void Unknown::Graphics::Animation::attemptAdvanceFrame() {
+    if(done)
+        return;
+
+	AnimationFrame currentFrame = frames[currentFrameIndex];
+
+	if (!hasInit)
+	{
+		frameTimer.resetTimer();
+		frameTimer.timerSpeed = currentFrame.delayms;
+		hasInit = true;
+	}
+
+
+	if (frameTimer.isTickComplete())
+	{
+		currentFrameIndex++;
+
+		if (currentFrameIndex == frames.size() - 1g) {
+            if (loop) {
+                currentFrameIndex = 0;
+            } else {
+                done = true;
+            }
+        }
 
 		frameTimer.timerSpeed = frames[currentFrameIndex].delayms;
 	}
---
+++ Unknown 1.0++/Entity/AnimationRenderComponent.cpp
@@ -0,0 +1,15 @@
+//
+// Created by cub3d on 12/08/18.
+//
+
+#include "AnimationRenderComponent.h"
+
+Unknown::AnimationRenderComponent::AnimationRenderComponent(Unknown::Graphics::Animation animation1) : animation(animation1) {}
+
+void Unknown::AnimationRenderComponent::render(const Unknown::Entity &ent, double xoff, double yoff) const {
+    animation.drawNoAdvance(ent.position.x, ent.position.y, ent.angle);
+}
+
+void Unknown::AnimationRenderComponent::update(Unknown::Entity &ent) {
+    animation.attemptAdvanceFrame();
+}
